840756d16a172353ae17bb267d59d99267296227
Fabio Pontes <faabiopontes@gmail.com>
2023-03-28 18:42:15 -0300

    feat: PHPInfo route and working Debug

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
